41e276e10a702cc80f2c706d543d8f01e2853852
Danysoftdev <danysoftdev@gmail.com>
2025-04-11 16:32:39 -0500

    Agregado endpint principal

--- src/main/java/com/eam/springboot_docker/SpringbootDockerApplication.java
+++ src/main/java/com/eam/springboot_docker/SpringbootDockerApplication.java
@@ -3,8 +3,14 @@ package com.eam.springboot_docker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
 public class SpringbootDockerApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hola desde Spring Boot con Docker!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerApplication.class, args);
